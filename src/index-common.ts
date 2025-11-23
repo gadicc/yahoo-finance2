ef7741af35163d1d77355111ace2e55e2a26b554
Gadi Cohen <dragon@wastelands.net>
2025-11-23 14:53:23 +0200

    fix(pkg): no code - just patch version bump

--- src/index-common.ts
+++ src/index-common.ts
@@ -24,7 +24,6 @@ import screener from "./modules/screener.js";
 // other
 import quoteCombine from "./other/quoteCombine.js";
 
-//
 console.warn(
   "[yahoo-finance2] v2 is no longer maintained nor supported.  Please migrate to v3. " +
     "See https://github.com/gadicc/yahoo-finance2/blob/dev/docs/UPGRADING.md for details.",
